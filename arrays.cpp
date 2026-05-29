d81652148726beeb5cfe3e878161b34abe5c0595
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-05-29 08:15:59 +0530

    Initial DSA setup

---
+++ arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+//traversal of an array
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    for(int i = 0; i < 5; i++){
+        cout << arr[i] << " ";
+
+    }
+    cout << endl;
+    return 0;
+}
