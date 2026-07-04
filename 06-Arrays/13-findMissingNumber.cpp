f498ad99cbf9f76bd0b74fe58f62d5515b999bb1
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-07-04 16:43:19 +0530

    Practiced questions on Arrays

---
+++ 06-Arrays/13-findMissingNumber.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    vector<int> hashArr(n+1, 0);
+    for(int i = 0; i<n; i++){
+        hashArr[arr[i]] = 1;
+    }
+    for(int i = 0; i<=n; i++){
+        if(hashArr[i] == 0){
+            cout << "The missing number is: " << i << endl;
+            break;
+        }
+    }
+    return -1;
+}
