73f25eeec2ef61cd3137316f55213c306b59fa76
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-07-04 16:53:18 +0530

    Practiced questions on Arrays

--- 06-Arrays/13-findMissingNumber.cpp
+++ 06-Arrays/13-findMissingNumber.cpp
@@ -5,7 +5,7 @@ int main(){
     int n;
     cout << "Enter the size of the array: ";
     cin >> n;
-    int arr[n];
+    vector<int> arr(n);
     cout << "Enter the elements of the array: ";
     for (int i = 0; i < n; i++)
     {
