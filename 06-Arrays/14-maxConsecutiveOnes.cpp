e6ed90db6c0bd579ba73689bfb5e549e69a0a315
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-07-05 10:19:18 +0530

    Practiced questions on Arrays

---
+++ 06-Arrays/14-maxConsecutiveOnes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int maxi = 0;
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == 1)
+        {
+            cnt++;
+            maxi = max(maxi, cnt);
+        }
+        else
+        {
+            cnt = 0;
+        }
+    }
+    cout << "The maximum number of consecutive 1's is: " << maxi << endl;
+}
