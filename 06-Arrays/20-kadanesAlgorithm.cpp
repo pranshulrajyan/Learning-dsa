b8439cb339148f46e5c86119745f0d51daa3db6f
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-07-14 16:35:16 +0530

    Kadane's Algorithm

--- 06-Arrays/20-kadanesAlgorithm.cpp
+++ 06-Arrays/20-kadanesAlgorithm.cpp
@@ -1,17 +1,33 @@
-#include<iostream>
-#include<climits>
+#include <iostream>
+#include <climits>
 using namespace std;
-int main(){
-    int arr[10] = {1,2,3,-4,5,2,7,11,-3,8};
+int main()
+{
+    int arr[10] = {1, 2, 3, -4, 5, 2, 7, 11, -3, 8};
     int sum = 0;
     int maxi = INT_MIN;
-    for(int i = 0; i<10; i++){
+    int start = 0, end = 0, tempstart = 0;
+    for (int i = 0; i < 10; i++)
+    {
         sum += arr[i];
-        maxi = max(sum, maxi);
-        if( sum <0){
+        if (sum > maxi)
+        {
+            maxi = sum;
+            start = tempstart;
+            end = i;
+        }
+        if (sum < 0)
+        {
             sum = 0;
+            tempstart = i + 1;
         }
     }
     cout << "The Sum of subarray with largest Sum is: " << maxi << endl;
+    cout << "The subarray is: ";
+    for (int i = start; i < end; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
