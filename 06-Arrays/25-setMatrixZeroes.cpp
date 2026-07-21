52fc8454b6ea1e4ddeb1649c9625b5639bab809e
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-07-21 21:06:43 +0530

    Set Matrix Zeroes

---
+++ 06-Arrays/25-setMatrixZeroes.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<vector<int>> matrix = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+    int col0 = 1;
+    int n = matrix.size();
+    int m = matrix[0].size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                matrix[i][0] = 0;
+
+                if (j != 0)
+                {
+                    matrix[0][j] = 0;
+                }
+                else
+                {
+                    col0 = 0;
+                }
+            }
+        }
+    }
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = 1; j < m; j++)
+        {
+            if (matrix[i][j] != 0)
+            {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+    if (col0 == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i][0] = 0;
+        }
+    }
+    if (matrix[0][0] == 0)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[0][j] = 0;
+        }
+    }
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<m; j++){
+            cout << matrix[i][j] << endl;
+        }
+    }
+    return 0;
+}
