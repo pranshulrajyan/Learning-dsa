23b3462f9257b9d9dcfb9a7d35e5d50df739dcbf
Pranshul Rajyan <rajyanpranshul@gmail.com>
2026-07-22 17:11:15 +0530

    Rotate Matrix by 90 degrees

---
+++ 06-Arrays/26-rotateMatrix.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector<vector<int>> matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    int n = matrix.size(); // it is a square matrix.
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            swap(matrix[i][j], matrix[j][i]);
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        reverse(matrix[i].begin(), matrix[i].end());
+    }
+    cout << "The rotated matrix is: ";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
